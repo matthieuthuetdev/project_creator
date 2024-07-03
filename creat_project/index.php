ed8dc9157c5a7441dd1a39977f32d6dd0c3866c3
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-03 16:49:41 +0200

    débyt dyyo

---
+++ creat_project/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "Créé un nouveau projet. \n Saisisez le chemain du dossier dans le quelle vous souhaitez créé votre projet.";
+$chemain = readline();
+echo $chemain;
--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-echo "  1 : créé un nouveau projet ? \n 2 : modifier la structure des fichier créé pour chaque projet ? (Bientôt disponible) \n 3 : m'envoyer votre avi sur cette outil ? \n 3 : rechèrcher des mise a joure \n que souhaitez vous faire ?";
+echo "  1 : créé un nouveau projet ? \n 2 : modifier la structure des fichier créé pour chaque projet ? (Bientôt disponible) \n 3 : m'envoyer votre avi sur cette outil ? \n 3 : rechèrcher des mise a joure \n que souhaitez vous faire ? (1)";
+$action = readline();
+echo $action;
+if ($action === "") {
+    $action = "1";
+}
+if ($action == 1) {
+    require "./creat_project/index.php";
+} else {
+    echo "  cette action n'est pas encor disponible, elle le sera dans une future mise a joure ";
+}
