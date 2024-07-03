eda437ed2cd31c9916bcde5ce9e213697a8787aa
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-03 11:54:30 +0200

    création du menu d'accueil

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+echo "  1 : créé un nouveau projet ? \n 2 : modifier la structure des fichier créé pour chaque projet ? (Bientôt disponible) \n 3 : m'envoyer votre avi sur cette outil ? \n 3 : rechèrcher des mise a joure \n que souhaitez vous faire ?";
