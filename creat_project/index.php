d6d04a3ad9a87351c9c940b95eea0598e8a26490
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-30 16:57:12 +0200

    f

--- creat_project/index.php
+++ creat_project/index.php
@@ -1,4 +1,6 @@
 <?php
-echo "Créé un nouveau projet. \n Saisisez le chemain du dossier dans le quelle vous souhaitez créé votre projet.";
-$chemain = readline();
-echo $chemain;
+echo "Créé un nouveau projet. \n saisisez le nom du projet :";
+$name = readline();
+echo $name;
+echo "Saisisez le chemain du dossier dans le quelle vous souhaitez créé votre projet (si le dossier cible porte déjà le nom du projet le projet sera créé à l'intérieur, dans le cas échèant le dossier portant le nom de votre projet sera créé) :";
+$path = readline();
